f71657d32a93c60ce6751ba89f4c7a1a9a9d5ef3
PatrickCarillo <p.carillo17@googlemail.com>
2026-06-04 20:32:39 +0200

    Initial commit: CategoryClassifier

---
+++ DataOrganizer/CategoryClassifier.cs
@@ -0,0 +1,13 @@
+﻿namespace DataOrganizer;
+
+public class CategoryClassifier
+{
+    private Dictionary<string, string> _categories =  new ()
+    {
+        {"Pictures",  new[] { ".jpg", ".jpeg", ".png", ".gif",}},
+        {"Documents", new[] { ".pdf", ".docx", ".odt", ".xls", ".txt"}},
+        {"Music", new[]}
+        {"Videos", new[] }
+    }
+    
+}
