344152fad4a383079edd83b28a6ec8dc0d45d129
PatrickCarillo <p.carillo17@googlemail.com>
2026-06-05 11:59:43 +0200

    CategoryClassifier structure implemented

--- DataOrganizer/CategoryClassifier.cs
+++ DataOrganizer/CategoryClassifier.cs
@@ -2,12 +2,29 @@
 
 public class CategoryClassifier
 {
-    private Dictionary<string, string> _categories =  new ()
+    // List of the data type categories
+    private Dictionary<string, string[]> _categories =  new () // string[] as array in the dict
     {
-        {"Pictures",  new[] { ".jpg", ".jpeg", ".png", ".gif",}},
-        {"Documents", new[] { ".pdf", ".docx", ".odt", ".xls", ".txt"}},
-        {"Music", new[]}
-        {"Videos", new[] }
+        {"Pictures",  new[] { ".jpg", ".jpeg", ".png", ".gif"} },
+        {"Documents", new[] { ".pdf", ".docx", ".odt", ".xls", ".txt"} }, // Add missing possible types
+        {"Music", new[] {".wav", ".wma", ".mp3"} }, // Adjust the types
+        {"Videos", new[] {".mp4", ".mpeg", ".avi"} },
+    };
+    // Reads filename extension and returns category
+    public string GetCategory(string extension)
+    {
+        extension = extension.ToLower(); 
+
+
+        foreach (var entry in _categories)
+        {
+
+            if (entry.Value.Contains(extension))
+            {
+                return entry.Key;
+            }
+        }
+
+        return "Misc.";
     }
-    
 }
--- DataOrganizer/Program.cs
+++ DataOrganizer/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using DataOrganizer;
+
+var mapper = new CategoryClassifier();
+Console.WriteLine(mapper.GetCategory(".jpg"));
+Console.WriteLine(mapper.GetCategory(".pdf"));
+Console.WriteLine(mapper.GetCategory(".xyz"));
